2bfdb183ebe32ef5cd6788d0552181c43f12a684
Melowe <51891075+Mel-Lowe@users.noreply.github.com>
2023-05-01 11:47:21 +0100

    added Synonym component

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
   console.log(props.meaning);
@@ -7,6 +8,7 @@ export default function Meanings(props) {
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>{props.meaning.definition}</p>
       <p className="fst-italic">{props.meaning.example}</p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
